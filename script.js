ebddc90e57de200213ccaed430fd343a42847f87
ishaanmaheshwari <236671684+Procedure-Alpha@users.noreply.github.com>
2026-02-24 10:26:12 +0530

    Lecture-3

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-    var sayhello = function() {
-        alert("Hello");
-    }
-    sayhello();
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if (sym1 === sym2) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+let largest = (x > y) ? ((x > z) ? x : z) : ((y > z) ? y : z);
+console.log(largest);
+
+var count;
+document.write("Starting Loop" + "<br>");
+for(count =0;count<10;count++){
+    document.write("Current count: " + count + "<br>");
+}
+document.write("Loop stopped!" + "<br>");
---
+++ script1.js
@@ -0,0 +1,21 @@
+let a = 10, b=20, linebreak = "<br>", result;
+document.write("a = " + a);
+document.write(linebreak);
+document.write("b = " + b);
+document.write(linebreak);
+document.write("a + b = ")
+result = a + b;
+document.write(result)
+document.write(linebreak);
+document.write("a - b = ")
+result = a - b;
+document.write(result)
+document.write(linebreak);
+document.write("a * b = ")
+result = a * b;
+document.write(result)
+document.write(linebreak);
+document.write("a / b = ")
+result = a / b;
+document.write(result)
+document.write(linebreak);
