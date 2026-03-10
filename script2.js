6d79cfc0ca940a02f325ce643359c3170740841c
ishaanmaheshwari <236671684+Procedure-Alpha@users.noreply.github.com>
2026-03-10 09:37:09 +0530

    Lecture-4

---
+++ script2.js
@@ -0,0 +1,16 @@
+let collection = [
+    {},
+    [],
+    true,
+    function () { },
+    new Date()
+]
+console.log(collection);
+collection.teacher = "John";
+collection.phone = 1234567890;
+console.log(collection);
+trainerinfo = ["John", 1234567890, "JavaScript"];
+console.log(trainerinfo.length);
+trainerinfo.contact = "john@example.com";
+console.log(trainerinfo.length);
+console.log(trainerinfo);
