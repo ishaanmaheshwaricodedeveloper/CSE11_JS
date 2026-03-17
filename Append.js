4f5cb211e6e10a79b359febd6da809bbfa466ce3
ishaanmaheshwari <236671684+Procedure-Alpha@users.noreply.github.com>
2026-03-17 09:33:03 +0530

    Lecture-8

---
+++ Append.js
@@ -0,0 +1,5 @@
+const heading = document.createElement("h2");
+const bodytag = document.getElementsByTagName("body");
+bodytag[0].append(heading);
+heading.innerHTML = "This is a heading created by JavaScript";
+
